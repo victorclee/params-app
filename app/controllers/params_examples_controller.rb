8961d2890b3579ee34c28170270fe8cf03149e11
victorclee <victorchristopherlee@gmail.com>
2017-05-04 21:26:22 -0500

    added forms param

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -2,4 +2,14 @@ class ParamsExamplesController < ApplicationController
   def query
     @message = params["my_message"]
   end
+
+  def form_show
+
+  end
+
+  def form_send
+    @message = params["form_message"]
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get '/query_params' => 'params_examples#query'
   
+
+  get '/form_show_params' => 'params_examples#form_show'
+  post '/form_send_params' => 'params_examples#form_send'
+
 end
